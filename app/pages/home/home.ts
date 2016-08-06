4f51a003f687bb71712ae633947e361fde8f13ae
daveozoalor <daveozoalor@gmail.com>
2016-08-06 09:16:11 +0100

    fixed saveItem() bug

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -3,15 +3,15 @@ import {Modal, NavController} from 'ionic-angular';
 import {AddItemPage} from '../add-item/add-item';
 import {ItemDetailPage} from '../item-detail/item-detail';
 import {Data} from '../../providers/data/data';
-
+ 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
 })
 export class HomePage {
-	
+ 
   private items = [];
  
-    constructor(private nav: NavController, private dataService: Data) {
+  constructor(private nav: NavController, private dataService: Data) {
  
     this.dataService.getData().then((todos) => {
  
@@ -22,18 +22,32 @@ export class HomePage {
     });
  
   }
-  
-  
-  
-  //handle adding new item
-    constructor(private nav: NavController, private dataService: Data) {
  
-    this.dataService.getData().then((todos) => {
+  addItem(){
  
-        if(todos){
-            this.items = JSON.parse(todos); 
-        }
+    let addModal = Modal.create(AddItemPage);
+ 
+    addModal.onDismiss((item) => {
+ 
+      if(item){
+        this.saveItem(item);
+      }
  
     });
  
+    this.nav.present(addModal);
+ 
   }
+ 
+  saveItem(item){
+    this.items.push(item);
+    this.dataService.save(this.items);
+  }
+ 
+  viewItem(item){
+    this.nav.push(ItemDetailPage, {
+      item: item
+    });
+  }
+ 
+}
